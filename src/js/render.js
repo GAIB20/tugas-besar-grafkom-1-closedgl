7855954a7b77da9c5236eecdca3973778b2a0a2c
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2024-03-20 18:03:36 +0700

    chore(render): refactor gl create program feat(render): add draw shape and clear canvas
    Co-Authored-by: bill27 <13521064@std.stei.itb.ac.id>
    Co-Authored-by: yuujin-Q <13521074@mahasiswa.itb.ac.id>

--- src/js/render.js
+++ src/js/render.js
@@ -1,3 +1,49 @@
+const createShader = (shaderType, shaderText) => {
+  let shader = gl.createShader(shaderType);
+
+  gl.shaderSource(shader, shaderText);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(
+      "err: ",
+      gl.getShaderInfoLog(shader)
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+};
+
+function createProgram(vertexShaderText, fragmentShaderText) {
+  const vertexShader = createShader(gl.VERTEX_SHADER, vertexShaderText);
+  const fragmentShader = createShader(gl.FRAGMENT_SHADER, fragmentShaderText);
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error("err: ", gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return;
+  }
+
+  gl.validateProgram(program);
+  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+    console.error("err: ", gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return;
+  }
+
+  gl.deleteShader(vertexShader);
+  gl.deleteShader(fragmentShader);
+
+  return program;
+}
+
 const baseRender = (
   gl,
   shaderProgram,
@@ -28,9 +74,30 @@ const baseRender = (
 };
 
 const renderVertex = (program, arrData = [], arrSize = 3) => {
+  if (!gl) alert("WebGL not supported")
   baseRender(gl, program, arrData, arrSize, "a_position");
 };
 
 const renderColor = (program, arrData = [], arrSize = 3) => {
+  if (!gl) alert("WebGL not supported")
   baseRender(gl, program, arrData, arrSize, "vertColor");
+};
+
+/**
+ * Draw a new shape based on given shapeType and push it to currShapes
+ * */
+const drawShape = (currShapes, shapeType, x, y) => {
+  switch (shapeType) {
+    case SHAPE.LINE:
+      currShapes.line.push(new Line(x, y));
+      break;
+
+    default:
+      break;
+  }
+};
+
+const clearCanvas = () => {
+  if (!gl) alert("WebGL not supported");
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 };
