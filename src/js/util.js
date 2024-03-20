bbf0cc2b325188e540d656e7fdd8610e5e7a97cd
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2024-03-20 18:02:34 +0700

    feat(util): on mouse move, get cursor position, flatten matrix
    Co-Authored-by: bill27 <13521064@std.stei.itb.ac.id>
    Co-Authored-by: yuujin-Q <13521074@mahasiswa.itb.ac.id>

--- src/js/util.js
+++ src/js/util.js
@@ -1,3 +1,15 @@
+/**
+ * Flatten the given matrix
+ * @returns Float32Array
+ * */ 
+const flattenMatrix = (matrix) => {
+  if (Array.isArray(matrix[0])) {
+    return new Float32Array(matrix.flat());
+  } else {
+    return new Float32Array(matrix);
+  }
+};
+
 const convertToWGLCoordinate = (canvas, x, y) => {
   const position = canvas.getBoundingClientRect();
   const [w, h] = [position.width, position.height];
@@ -10,10 +22,66 @@ const convertToWGLCoordinate = (canvas, x, y) => {
 };
 
 const parseRGB = (rgb) => {
-  //convert rgb to 0 to 1 form
+  //convert rgb HEX to [0-1]
   const r = parseInt(rgb.substr(1, 2), 16) / 255;
   const g = parseInt(rgb.substr(3, 2), 16) / 255;
   const b = parseInt(rgb.substr(5, 2), 16) / 255;
 
   return { r, g, b };
+};
+
+// /**
+//  * Get the centroid from the given matrix shape
+//  * */
+// const getCentroid = (matrix) => {
+//   let x = 0;
+//   let y = 0;
+//   let totalVertex = matrix.length;
+
+//   for (i = 0; i < totalVertex; i++) {
+//     x += matrix[i][0];
+//     y += matrix[i][1];
+//   }
+
+//   x = x / totalVertex;
+//   y = y / totalVertex;
+
+//   return [x, y];
+// };
+
+/**
+ * Returns user current cursor position
+ * */
+const getCursorPosition = (canvas, e) => {
+  const position = canvas.getBoundingClientRect();
+
+  const x = e.clientX - position.x;
+  const y = e.clientY - position.y;
+
+  return { x, y };
+};
+
+/**
+ * Gets the latest object of the given shape type
+ * Animate drawing by updating last vertex position (LINE and RECTANGLE)
+ * */
+const onMouseMove = (currShapes, shapeType, x, y) => {
+  let [wglWidth, wglHeight] = convertToWGLCoordinate(canvas, x, y);
+
+  if (shapeType === SHAPE.LINE) {
+    const len = currShapes[SHAPE.LINE].length;
+    currShapes[SHAPE.LINE][len - 1].updateLastVertexPosition(
+      wglWidth,
+      wglHeight
+    );
+    return;
+  }
+
+  if (shapeType === SHAPE.RECTANGLE) {
+    const len = currShapes[SHAPE.RECTANGLE].length;
+    currShapes[SHAPE.RECTANGLE][len - 1].updateLastVertexPosition(
+      wglWidth,
+      wglHeight
+    );
+  }
 };
